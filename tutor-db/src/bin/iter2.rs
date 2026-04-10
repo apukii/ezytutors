0f13f8aec515f85ddd66ed388020e2ecd0b5e859
apuki <l1nefeed@gmail.com>
2026-04-10 20:01:44 +0900

    20260410 집에서 커밋

---
+++ tutor-db/src/bin/iter2.rs
@@ -0,0 +1,42 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use std::env;
+use std::io;
+use std::sync::Mutex;
+
+#[path = "../iter2/handlers.rs"]
+mod handlers;
+#[path = "../iter2/models.rs"]
+mod models;
+#[path = "../iter2/routes.rs"]
+mod routes;
+#[path = "../iter2/state.rs"]
+mod state;
+
+use routes::*;
+use state::AppState;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    // App 상태를 구현한다.
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've aleady asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    // 앱을 구성하고 라우트를 구성한다.
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+
+    // HTTP 서버를 시작한다.
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
---
+++ tutor-db/src/database.sql
@@ -0,0 +1,19 @@
+/* 테이블이 존재하면 삭제한다 */
+drop table if exists ezy_course_c4;
+/* 테이블을 생성한다 */
+/* 노트: 마지막 필드 뒤에 콤마를 입력해서는 안 된다. */
+create table ezy_course_c4
+(
+	course_id serial primary key,
+	tutor_id INT not null,
+	course_name varchar(140) not null,
+	posted_time TIMESTAMP default now()
+);
+
+/* 테스트를 위한 시드 데이터를 로드한다. */
+insert into ezy_course_c4
+	(course_id, tutor_id, course_name, posted_time)
+values (1, 1, 'First course', '2020-12-17 05:40:00');
+insert into ezy_course_c4
+	(course_id, tutor_id, course_name, posted_time)
+values (2, 1, 'Second course', '2020-12-18 05:45:00');
